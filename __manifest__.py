17bd10a40211cc19d2ab1506d997d8e86485367b
Makalae <rubio.jigem22@zaragoza.salesianos.edu>
2023-11-20 09:41:17 +0100

    second commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+	'name': "cerveza",
+    'summary': """cerveza """,
+    'description': """
+        módulo para la gestión de venta y distribución de cerveza
+    """,
+    'author': "Makalae",
+
+    'category': 'odoo',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
