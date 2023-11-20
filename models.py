fb76d6251e213aba60f663a4046f391f95b2a3b8
Makalae <rubio.jigem22@zaragoza.salesianos.edu>
2023-11-20 10:23:21 +0100

    views trees

--- models.py
+++ models.py
@@ -8,28 +8,15 @@ class cerveza (models.Model):
     alcohol_rate = fields.Float(string='Contenido de alcohol %')
     precio_unidad = fields.Float(string="Precio por unidad")
     volume_per_unite = fields.Float(string='Volumen alcohol (ml)')
-    available = fields.Boolean(string='Disponible', compute='_compute_disponible')
+    available = fields.Boolean(string='Disponible')
     productionPack = fields.One2many(comodel_name='cerveza.lote', inverse_name='cerveza_id')
     distributor_id = fields.Many2one(comodel_name='cerveza.distribuidor', string = 'distributor_id')
-    
-    #@api.depends('inventario')
-    #def _compute_disponible(self):
-        #for record in self:
-            #record.available = True if record.inventario > 0 else False
-    
-     # Filtros de búsqueda personalizados
-    #@api.model
-    #def _search_cervezas_agotadas(self):
-        #return [('disponible', '=', False)]
-
-    #@api.model
-    #def _search_cervezas_disponibles(self):
-        #return [('disponible', '=', True)]
     
     
 class loteProduccion(models.Model):
     _name='cerveza.lote'
     cerveza_id =fields.Many2one(comodel_name='cerveza.cerveza', string='cerveza_id')
+    date_start = fields.Date()
     date_end = fields.Date()
     quantity = fields.Integer()
     state = fields.Selection([ ('proceso', 'En Proceso'), ('completo', 'Completado'), ('espera' , 'En espera de empaquetado')], string = 'Estado')
@@ -49,6 +36,6 @@ class empaquetado(models.Model):
 
 class distribuidor(models.Model):
     _name='cerveza.distribuidor'
-    name=fields.Char()
-    phone=fields.Char()
+    name=fields.Char(string='Nombre')
+    phone=fields.Char(string='Teléfono')
     beers_supplied=fields.One2many(comodel_name='cerveza.cerveza', inverse_name='distributor_id')
